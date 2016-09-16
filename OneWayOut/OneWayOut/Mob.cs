8731adfecb532738989ead66bee673dc4a6d812a
thomas <twt6012@g.rit.edu>
2016-09-16 15:50:52 -0400

    set up classes

---
+++ OneWayOut/OneWayOut/Mob.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace OneWayOut
+{
+    class Mob
+    {
+        public string name { get; set; }
+        public int speed { get; set; }
+        public Rectangle position  { get; set; }
+        public int damage { get; set; }
+    }
+}
---
+++ OneWayOut/OneWayOut/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace OneWayOut
+{
+    class Player
+    {
+        public string name { get; set; }
+        public int arrowSupply { get; set; }
+        public int health { get; set; }
+        public Rectangle position { get; set; }
+    }
+}
