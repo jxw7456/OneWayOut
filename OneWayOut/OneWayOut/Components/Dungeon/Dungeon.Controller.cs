a15f54c78a919d1b7888228f15af76d9c4b5bff7
LAB Nguyen (RIT Student) <hhn2884@ad.rit.edu>
2016-09-25 16:58:34 -0400

    Tile generator initialized

---
+++ OneWayOut/OneWayOut/Components/Dungeon/Dungeon.Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWayOut.Components.Dungeon
+{
+    partial class Dungeon
+    {
+        public Dungeon(int x, int y, int width, int height) : base(x, y, width, height)
+        {
+
+        }
+    }
+}
