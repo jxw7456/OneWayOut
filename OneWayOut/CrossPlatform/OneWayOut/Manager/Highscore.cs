6b581cb3fe6ed5ec6648c3a9f8b8ea1accc17993
Louis Vichy <lgvichy@gmail.com>
2016-10-17 16:49:02 -0400

    Remove close as  will close it for you

--- OneWayOut/CrossPlatform/OneWayOut/Manager/Highscore.cs
+++ OneWayOut/CrossPlatform/OneWayOut/Manager/Highscore.cs
@@ -9,41 +9,39 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using OneWayOut;
 using Microsoft.Xna.Framework.Content;
+
 namespace OneWayOut
 {
-    class Highscore
-    {
-        List<string> entireFile = new List<string>();
+	class Highscore
+	{
+		List<string> entireFile = new List<string> ();
 
-        SpriteFont font;
+		SpriteFont font;
 
-        Vector2 pos = new Vector2(600, 180);
+		Vector2 pos = new Vector2 (600, 180);
 
-        public string line;
-        public Highscore(ContentManager Content)
-        {
-            font = Content.Load<SpriteFont>(@"fonts/bold");
-        }
+		public string line;
 
-        public void readScore()
-        {
-            try
-            {
-                using (var highScore = new StreamReader("highscore.txt"))
-                {
-                    line = highScore.ReadToEnd();
-                    highScore.Close();
-                }
-            }
-            catch (Exception e)
-            {
+		public Highscore (ContentManager Content)
+		{
+			font = Content.Load<SpriteFont> (@"fonts/bold");
+		}
 
-            }
-        }
+		public void readScore ()
+		{
+			try {
+				using (var highScore = new StreamReader ("highscore.txt")) {
+					line = highScore.ReadToEnd ();
 
-        public void DrawScore(SpriteBatch sb)
-        {
-            sb.DrawString(font, line, pos, Color.Red);
-        }
-    }
+				}
+			} catch (Exception e) {
+
+			}
+		}
+
+		public void DrawScore (SpriteBatch sb)
+		{
+			sb.DrawString (font, line, pos, Color.Red);
+		}
+	}
 }
